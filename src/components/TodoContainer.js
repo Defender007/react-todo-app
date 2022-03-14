dcf3264e97f6f63701b4e1d172c2992cde466822
Defender007 <mydjangopythonprojects@gmail.com>
2022-03-14 22:36:27 +0100

    state elevation

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,5 +1,6 @@
 import React from "react";
 import TodosList from "./TodosList";
+import Header from "./Header";
 
 class TodoContainer extends React.Component {
   state = {
@@ -21,11 +22,20 @@ class TodoContainer extends React.Component {
       },
     ],
   };
+
+  handleChange = () => {
+    console.log("clicked");
+  };
+
   render() {
     return (
       <div>
-      <TodosList todos={this.state.todos} />
-    </div>
+        <Header />
+        <TodosList
+          todos={this.state.todos}
+          handleChangeProps={this.handleChange}
+        />
+      </div>
     );
   }
 }
